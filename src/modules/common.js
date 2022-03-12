a71af983c749862b48281b7efcd4364a729069a8
Cain Atkinson <yellowsink@riseup.net>
2022-03-12 16:45:00 +0000

    constants woa

--- src/modules/common.js
+++ src/modules/common.js
@@ -1,6 +1,13 @@
 import webpack from "./webpack";
 
+const bothConstants = webpack.findByPropsAll("app_name");
+
+const EN_APP_NAME = "Kaiheila"
+
 export default {
   React: webpack.findByProps("createElement"),
   lodash: _,
+
+  constantsCN: bothConstants.find(c => c.app_name !== EN_APP_NAME),
+  constantsEN: bothConstants.find(c => c.app_name === EN_APP_NAME),
 };
