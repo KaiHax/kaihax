4bbfeceee7c4fd1e9a57f29fe32f51ac9fdb1404
Cain Atkinson <yellowsink@riseup.net>
2022-03-13 11:53:49 +0000

    fix translation bug in nowFriends.js

--- src/en_translate/dom/nowFriends.js
+++ src/en_translate/dom/nowFriends.js
@@ -1,15 +1,15 @@
 import utils from "../../utils";
-import CLASS_NAMES from "./CLASS_NAMES.json"
+import CLASS_NAMES from "./CLASS_NAMES.json";
 
 export default () => {
   const elem = document.getElementsByClassName(CLASS_NAMES.message_oneline)[0];
-  if (!elem|| !elem.lastChild.innerText.startsWith("你与")) return;
+  if (!elem || !elem.lastChild.innerText.startsWith("你与")) return;
 
   const friendName = utils.reactFiberWalker(
     utils.getFiber(elem),
-    "msgInfo",
+    "currentChat",
     true
-  )?.pendingProps.msgInfo.author.username;
+  )?.pendingProps.currentChat.target_info.nickname;
 
-  elem.lastChild.innerText = `You and ${friendName} are now friends.`
-}
+  elem.lastChild.innerText = `You and ${friendName} are now friends.`;
+};
